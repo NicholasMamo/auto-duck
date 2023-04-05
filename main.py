3f0d9ee74ed2e66fd05133fbaf5cadbf84a16e65
Nicholas Mamo <nicholasmamo@gmail.com>
2023-04-05 21:31:29 +0200

    `main.py`: add the configuration function

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
 #!/usr/bin/env python
 """
+CONFIGURATION
+
+The configuration variables are defined globally, and then set in the :func:`config` function once all the fade functions have been defined.
+This is only done to place the configuration at the top of the file.
+"""
+
+DURATION = None
+DUCK = None
+STEPS = None
+FADE = None
+
+def config():
+    """
+    Configure the auto-ducker.
+    The function is defined here so that by the time it is called, the functions have all been defined.
+    """
+
+    # the number of milliseconds to spend fading
+    global DURATION
+    DURATION = 2000
+
+    # the minimum volume to which to duck music applications
+    global DUCK
+    DUCK = 0.4
+
+    # how detailed the auto-ducking; the higher the steps, the lower the performance but the more seamless the ducking
+    global STEPS
+    STEPS = 30
+
+    # the fade function
+    global FADE
+    FADE = bezier
+
 FADE FUNCTIONS
 """
 
