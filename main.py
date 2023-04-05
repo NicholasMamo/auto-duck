e2bdb41e921123bb998ede4858d58c151ee0cadb
Nicholas Mamo <nicholasmamo@gmail.com>
2023-04-05 21:30:59 +0200

    `main.py`: add the Bezier fade function

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python
+"""
+FADE FUNCTIONS
+"""
+
+def bezier(x):
+    """
+    Calculate the bezier coefficient.
+
+    :param x: The parameter value of the bezier function.
+    :type x: int
+
+    :return: The bezier coefficient.
+    :rtype: float
+    """
+
+    x = x / STEPS
+    return x * x * (3.0 - 2.0 * x) # bezier
