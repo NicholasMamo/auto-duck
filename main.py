48d97147c59eb69c329e5c0ac553a312f59c9c24
Nicholas Mamo <nicholasmamo@gmail.com>
2023-07-31 16:18:46 +0200

    Exclude buggy text-to-speech from ducking audio

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ async def main():
                 media_name = app.proplist.get('media.name', '').lower()
                 media_role = app.proplist.get('media.role', '').lower()
                 application_name = app.proplist.get('application.name', '').lower()
-                if ((media_name and media_name == 'playback') or 
+                if ((media_name and media_name == 'playback' and (not media_role or media_role == 'speech-dispatcher-espeak-ng')) or 
                     (media_role and media_role != 'music') or
                     (application_name and application_name == 'firefox')):
                     playback = True
